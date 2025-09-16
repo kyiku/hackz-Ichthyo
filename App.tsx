8a239f61d1010e6323650ff3bc203ee45f1ab4d5
kyiku <kyikutsu@gmail.com>
2025-09-17 04:45:37 +0900

    commit

--- App.tsx
+++ App.tsx
@@ -17,9 +17,6 @@ const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center font-mono p-4">
       <header className="mb-4 text-center">
-        <h1 className="text-4xl font-bold text-yellow-400 tracking-wider" style={{ textShadow: '2px 2px 0 #000' }}>
-          🎮 ARCADE ～ゲームセンター物語～
-        </h1>
       </header>
       <Game />
     </div>
